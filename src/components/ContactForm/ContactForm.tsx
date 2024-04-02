332549228ea4d1bc204d2b6630481ba6a8374fc7
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-04-02 23:13:38 +0200

    final contact form

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import { FC, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import { Formik, Form, Field, ErrorMessage, FormikHelpers } from 'formik';
 import * as Yup from 'yup';
 import axios from 'axios';
-import { sendEmail } from '@/utils/send-email';
 
 import styles from './ContactForm.module.scss';
 
@@ -12,35 +11,58 @@ export type FormData = {
   name: string;
   phone: string;
   email: string;
+  agreedToPolicy: boolean;
 };
 
 const ContactForm: FC = () => {
-
   const [message, setMessage] = useState<string | null>(null);
+  const [filled, setFilled] = useState({ name: false, phone: false, email: false });
+
+    useEffect(() => {
+    const interval = setInterval(() => {
+      ['name', 'phone', 'email'].forEach(field => {
+        const input = document.getElementById(field) as HTMLInputElement;
+        if (input && input.value) {
+          setFilled(f => ({ ...f, [field]: true }));
+        }
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFilled(prev => ({ ...prev, [name]: value.trim() !== '' }));
+  };
 
   const initialValues: FormData = {
     name: '',
     phone: '',
     email: '',
+    agreedToPolicy: false,
   };
 
   const validationSchema = Yup.object({
     name: Yup.string().required('Imię jest wymagane'),
     phone: Yup.string().required('Telefon jest wymagany'),
-    email: Yup.string().email('Nieprawidłowy adres email').required('Email jest wymagany'),
+    email: Yup.string().email('Nieprawidłowy адрес email').required('Email jest wymagany'),
+    agreedToPolicy: Yup.boolean()
+      .required('Wymagana zgoda')
+      .oneOf([true], 'Wymagana jest zgoda na przetwarzanie danych osobowych'),
   });
 
   const onSubmit = async (values: FormData, { setSubmitting, resetForm }: FormikHelpers<FormData>) => {
     setSubmitting(true);
     try {
-      // Используем axios для отправки данных на API-роут
       const response = await axios.post('/api/email', values);
       if (response.data.message === 'Email sent') {
-        setMessage('Otrzymałem Twoją wiadomość i wkrótce się z Tobą skontaktuję. Poczekaj chwilę :)');
+        setMessage('Otrzymałem Twoją wiadomość i wkrótce się з Tobą skontaktuję. Poczekaj chwilę :)');
+        resetForm({});
+        setFilled({ name: false, phone: false, email: false }); // Reset the filled state
       } else {
         throw new Error('Server responded with an error');
       }
-      resetForm({});
     } catch (error) {
       setMessage('Wystąpił błąd podczas wysyłania formularza');
     } finally {
@@ -50,6 +72,7 @@ const ContactForm: FC = () => {
       }, 5000);
     }
   };
+
 
   return (
     <>
@@ -62,19 +85,23 @@ const ContactForm: FC = () => {
         {({ isSubmitting }) => (
           <Form>
             <div className={styles.inputWrapper}>
-              <label htmlFor='name' className={styles.label}>Imię</label>
-              <Field id='name' name='name' type='text' className={`${styles.inputField} w-full rounded-md`} />
+              <label htmlFor='name' className={`${styles.label} ${filled.name ? styles.filled : ''}`}>Imię</label>
+              <Field id='name' name='name' type='text' className={`${styles.inputField} w-full rounded-md`} onBlur={handleBlur} />
               <ErrorMessage name='name' component='div' className={styles.error} />
             </div>
             <div className={styles.inputWrapper}>
-              <label htmlFor='phone' className={styles.label}>Telefon</label>
-              <Field id='phone' name='phone' type='tel' className={`${styles.inputField} w-full rounded-md`} />
+              <label htmlFor='phone' className={`${styles.label} ${filled.phone ? styles.filled : ''}`}>Telefon</label>
+              <Field id='phone' name='phone' type='tel' className={`${styles.inputField} w-full rounded-md`} onBlur={handleBlur} />
               <ErrorMessage name='phone' component='div' className={styles.error} />
             </div>
             <div className={styles.inputWrapper}>
-              <label htmlFor='email' className={styles.label}>E-mail</label>
-              <Field id='email' name='email' type='email' className={`${styles.inputField} w-full rounded-md`} />
+              <label htmlFor='email' className={`${styles.label} ${filled.email ? styles.filled : ''}`}>E-mail</label>
+              <Field id='email' name='email' type='email' className={`${styles.inputField} w-full rounded-md`} onBlur={handleBlur} />
               <ErrorMessage name='email' component='div' className={styles.error} />
+            </div>
+            <div className={styles.customCheckbox}>
+              <Field type="checkbox" name="agreedToPolicy" id="agreedToPolicy" />
+              <label htmlFor="agreedToPolicy">Wyrażam zgodę na wysyłanie informacji handlowych i reklamowych na podany przeze mnie adres e-mail</label>
             </div>
             <div>
               <button type='submit' className={styles.sentBtn} disabled={isSubmitting}>Wyślij wiadomość</button>
