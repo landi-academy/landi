2ae0331fd09b874127b563011b2f11f287e29e83
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-04-06 14:59:24 +0200

    fix layout

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { urlFor } from '@/libs/sanity';
 import styles from './Carousel.module.scss';
@@ -12,32 +13,45 @@ type Props = {
 };
 
 const Carousel: React.FC<Props> = ({ images }) => {
-  return (
-      <Slider
-        width="300px"
-        duration={30}
-        spacing={0}
-        pauseOnHover={true}
-        blurBorders={true}
-        blurBoderColor={'#ffffff'}
-        className={styles.slider}
-      >
-        {images.map((image) => (
-          <Slider.Slide key={image._key}>
-            <div className={styles.gridItem}>
-              <Image
-                className={styles.image}
-                src={urlFor(image).url()}
-                width={300}
-                height={300}
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                alt="Gallery image"
-              />
-            </div>
-          </Slider.Slide>
-        ))}
-      </Slider>
-  )
-}
+  const [isDesktop, setIsDesktop] = useState(false);
 
-export default Carousel
+  useEffect(() => {
+    const updateWindowWidth = () => {
+      setIsDesktop(window.innerWidth > 768); // Предположим, что десктоп начинается от 768px
+    };
+
+    window.addEventListener('resize', updateWindowWidth);
+    updateWindowWidth(); // Инициализация при монтировании
+
+    return () => window.removeEventListener('resize', updateWindowWidth);
+  }, []);
+
+  return (
+    <Slider
+      width="300px"
+      duration={30}
+      spacing={0}
+      pauseOnHover={true}
+      blurBorders={isDesktop} // Условно применяем blurBorders
+      blurBoderColor={'#ffffff'}
+      className={styles.slider}
+    >
+      {images.map((image) => (
+        <Slider.Slide key={image._key}>
+          <div className={styles.gridItem}>
+            <Image
+              className={styles.image}
+              src={urlFor(image).url()}
+              width={300}
+              height={300}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              alt="Gallery image"
+            />
+          </div>
+        </Slider.Slide>
+      ))}
+    </Slider>
+  );
+};
+
+export default Carousel;
