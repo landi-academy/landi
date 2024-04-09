e99f44aa2c47a46512068219264798517ff2fce7
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-04-09 20:55:17 +0200

    create webhook and checkout

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,56 +1,50 @@
 import Stripe from 'stripe';
 import { NextRequest, NextResponse } from 'next/server';
+import { v4 as uuidv4 } from 'uuid';
 import { sanityClient } from '@/libs/sanity';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
 });
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest, res: NextResponse) {
+  const reqBody = await req.text();
   const sig = req.headers.get('stripe-signature');
-  let event;
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+
+  let event: Stripe.Event;
 
   try {
-    const payload = await req.text(); // Используйте req.text() для получения тела запроса в виде строки
-
-console.log('Payload:', payload);
-console.log('Signature:', sig);
-
-    event = stripe.webhooks.constructEvent(payload, sig!, process.env.STRIPE_WEBHOOK_SECRET!);
-  } catch (err) {
-    if (err instanceof Error) {
-      return new NextResponse(`Webhook Error: ${err.message}`, { status: 400 });
-    }
-    return new NextResponse('Webhook Error: Unknown error', { status: 400 });
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
+  } catch (err: any) {
+    return new NextResponse(`Webhook Error: ${err.message}`, { status: 500 });
   }
 
   // Обработка события завершения сессии оплаты
   if (event.type === 'checkout.session.completed') {
-    const session = event.data.object;
-
-    // Генерируем уникальный URL
-    const uniqueURL = `https://landi-academy.pl/kurs/kurs-tajemnice-nanoplastii/session/${session.id}`;
-    console.log('Уникальный URL для доступа к курсу:', uniqueURL);
-
-    // Создаем документ для доступа к курсу в Sanity
-    const courseAccessDoc = {
-      _type: 'courseAccess',
-      courseId: 'prod_PsLvsSwZq534gt', // Укажите ID вашего курса
-      slug: {
-        _type: 'slug',
-        current: `kurs-${session.id}`, // Генерация уникального slug на основе session.id
-      },
-      createdAt: new Date().toISOString(),
-      stripePurchaseId: session.id,
-    };
+    const session = event.data.object as Stripe.Checkout.Session;
 
     // Сохраняем документ в Sanity
     try {
-      await sanityClient.create(courseAccessDoc);
-      console.log('Course access saved:', courseAccessDoc);
+        await sanityClient.create({
+        _type: 'courseAccess',
+        courseId: 'prod_PtUy08B8jxadVd', // Установите реальный идентификатор курса или извлеките его из сессии
+        slug: {
+          _type: 'slug',
+          current: uuidv4(),
+        },
+        createdAt: new Date().toISOString(),
+        stripePurchaseId: session.id,
+      });
+
+      return NextResponse.json('Event Received', {
+        status: 200,
+        statusText: 'Event Received',
+      });
     } catch (error) {
-      console.error('Error saving course access:', error);
-      return new NextResponse('Error saving course access', { status: 500 });
+      console.error('Ошибка при создании записи в Sanity:', error);
+      return new NextResponse('Ошибка при создании записи в Sanity', { status: 500 });
     }
   }
 
--- src/libs/sanity.ts
+++ src/libs/sanity.ts
@@ -4,9 +4,9 @@ import ImageUrlBuilder from "@sanity/image-url";
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET as string,
-  apiVersion: "2021-10-21",
-  useCdn: process.env.NODE_ENV === "production",
-  // token: process.env.SANITY_STUDIO_TOKEN,
+  apiVersion: "2023-10-16",
+  useCdn: false,
+  token: process.env.SANITY_STUDIO_TOKEN,
 });
 
 // export default sanityClient;
