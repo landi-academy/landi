dcfed404980f4d0da0c2b92c5df16d3ea1be5513
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-04-09 10:42:17 +0200

    del link

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -22,7 +22,7 @@ const Contact = async () => {
               <p className={styles.contactDescription}>{contact.contactDescription}</p>
             {/* <ContactForm /> */}
             <Link
-              href="https://buy.stripe.com/test_6oEeVrdBu3BZayscMP"
+              href="#"
               className={styles.buyLink}
             >
               Kupuj ze zniżką
