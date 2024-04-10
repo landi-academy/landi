9aeb6b2014032244c70f48f3fd6dd64c76e85377
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-04-10 19:13:37 +0200

    check static data

--- src/app/(web)/kurs/[session_id].tsx
+++ src/app/(web)/kurs/[session_id].tsx
@@ -13,35 +13,38 @@ const bigShoulders = Big_Shoulders_Display({ weight: ['400', '700'], subsets: ["
 const CoursePage = () => {
   const router = useRouter();
   const { session_id } = router.query; // Используйте session_id вместо slug
-  const [course, setCourse] = useState<Course | null>(null);
+  // const [course, setCourse] = useState<Course | null>(null);
 
 useEffect(() => {
   // Приведение session_id к строке, если это массив
   const sessionId = Array.isArray(session_id) ? session_id[0] : session_id;
   if (!sessionId) return;
-  const fetchCourse = async () => {
-    const data = await getCourseBySessionId(sessionId);
-    setCourse(data);
-  };
+  // const fetchCourse = async () => {
+  //   const data = await getCourseBySessionId(sessionId);
+  //   setCourse(data);
+  // };
 
-  fetchCourse();
+  // fetchCourse();
 }, [session_id]);
 
-  if (!course) {
-    return <div>Loading...</div>;
-  }
+  // if (!course) {
+  //   return <div>Loading...</div>;
+  // }
 
-  const pdfUrl = course.pdfFile && course.pdfFile.asset ? fileUrl(course.pdfFile.asset._ref) : null;
-  const videoUrl = course.videoFile && course.videoFile.asset ? fileUrl(course.videoFile.asset._ref) : null;
+  // const pdfUrl = course.pdfFile && course.pdfFile.asset ? fileUrl(course.pdfFile.asset._ref) : null;
+  // const videoUrl = course.videoFile && course.videoFile.asset ? fileUrl(course.videoFile.asset._ref) : null;
 
   return (
     <main className="course">
       <div className="container">
         <div className="courseWrapper">
-          <h1 className={`courseTitle ${bigShoulders.className}`}>{course.title}</h1>
+          {/* <h1 className={`courseTitle ${bigShoulders.className}`}>{course.title}</h1>
           <p className='courseDescription'>{course.description}</p>
           {videoUrl && <VideoComponent videoUrl={videoUrl} />}
-          {pdfUrl && <PdfLink pdfUrl={pdfUrl} />}
+          {pdfUrl && <PdfLink pdfUrl={pdfUrl} />} */}
+          <div>Тут будут мои данные на основании </div>
+          {/* add text with sessionId */}
+          <div>{session_id}</div>
         </div>
       </div>
     </main>
