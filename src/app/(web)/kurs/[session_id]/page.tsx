2a2fe2bb82c7131c3499ffacfc59dc9dc1348bc8
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-04-10 20:09:14 +0200

    back changes

--- src/app/(web)/kurs/[session_id]/page.tsx
+++ src/app/(web)/kurs/[session_id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useSearchParams, useParams } from 'next/navigation';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Big_Shoulders_Display } from "next/font/google";
 import PdfLink from '@/components/PdfLink/PdfLink';
@@ -11,44 +11,38 @@ import { Course } from '@/types/course';
 const bigShoulders = Big_Shoulders_Display({ weight: ['400', '700'], subsets: ["latin"] });
 
 const CoursePage = () => {
-  // const router = useRouter();
-  // const { session_id } = router.query;
-  // const session_id = usePathname(); // Используйте session_id вместо slug
-  // const [course, setCourse] = useState<Course | null>(null);
-
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // const session_id = searchParams.get('session_id');
-  const { session_id } = useParams();
+  const router = useRouter();
+  const { session_id } = router.query; // Используйте session_id вместо slug
+  const [course, setCourse] = useState<Course | null>(null);
 
 useEffect(() => {
   // Приведение session_id к строке, если это массив
   const sessionId = Array.isArray(session_id) ? session_id[0] : session_id;
   if (!sessionId) return;
-  // const fetchCourse = async () => {
-  //   const data = await getCourseBySessionId(sessionId);
-  //   setCourse(data);
-  // };
+  const fetchCourse = async () => {
+    const data = await getCourseBySessionId(sessionId);
+    setCourse(data);
+  };
 
-  // fetchCourse();
+  fetchCourse();
 }, [session_id]);
 
-  // if (!course) {
-  //   return <div>Loading...</div>;
-  // }
+  if (!course) {
+    return <div>Loading...</div>;
+  }
 
-  // const pdfUrl = course.pdfFile && course.pdfFile.asset ? fileUrl(course.pdfFile.asset._ref) : null;
-  // const videoUrl = course.videoFile && course.videoFile.asset ? fileUrl(course.videoFile.asset._ref) : null;
+  const pdfUrl = course.pdfFile && course.pdfFile.asset ? fileUrl(course.pdfFile.asset._ref) : null;
+  const videoUrl = course.videoFile && course.videoFile.asset ? fileUrl(course.videoFile.asset._ref) : null;
 
   return (
     <main className="course">
       <div className="container">
         <div className="courseWrapper">
-          {/* <h1 className={`courseTitle ${bigShoulders.className}`}>{course.title}</h1>
+          <h1 className={`courseTitle ${bigShoulders.className}`}>{course.title}</h1>
           <p className='courseDescription'>{course.description}</p>
           {videoUrl && <VideoComponent videoUrl={videoUrl} />}
-          {pdfUrl && <PdfLink pdfUrl={pdfUrl} />} */}
+          {pdfUrl && <PdfLink pdfUrl={pdfUrl} />}
           <div>Тут будут мои данные на основании </div>
-          {/* add text with sessionId */}
           <div>{session_id}</div>
         </div>
       </div>
