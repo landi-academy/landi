c5b25743f631292990fff47fa1d06ab616728636
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-04-10 15:30:20 +0200

    change fetch

--- src/app/(web)/kurs/[session_id].tsx
+++ src/app/(web)/kurs/[session_id].tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState, useEffect } from 'react';
 // import { createClient } from "next-sanity";
 import { Big_Shoulders_Display } from "next/font/google";
 import PdfLink from '@/components/PdfLink/PdfLink';
@@ -21,6 +23,10 @@ const CoursePage = async () => {
   // Убедитесь, что session_id является строкой
   const session_id = typeof router.query.session_id === 'string' ? router.query.session_id : null;
 
+  const [myCourse, setMyCourse] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
   if (!session_id) {
     // Обработайте случай, когда session_id не предоставлен или не является строкой
     return <div>Session ID is missing or invalid.</div>;
@@ -29,8 +35,42 @@ const CoursePage = async () => {
   // Используйте session_id для запроса данных курса
   const course = await getCourseBySessionId(session_id, sanityClient);
 
-  if (!course) {
+  // if (!course) {
+  //   return <div>Loading...</div>;
+  // }
+
+    useEffect(() => {
+    const loadCourse = async () => {
+      try {
+        const session_id = typeof router.query.session_id === 'string' ? router.query.session_id : null;
+        if (!session_id) {
+          throw new Error('Session ID is missing or invalid.');
+        }
+        const courseData = await getCourseBySessionId(session_id, sanityClient);
+        if (!courseData) {
+          throw new Error('Course not found.');
+        }
+        setMyCourse(courseData);
+      } catch (e: any) {
+        setError(e.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadCourse();
+  }, [router.query.session_id]);
+
+  if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!course) {
+    return <div>Course not found.</div>;
   }
 
   const pdfUrl = course.pdfFile && course.pdfFile.asset ? fileUrl(course.pdfFile.asset._ref) : null;
