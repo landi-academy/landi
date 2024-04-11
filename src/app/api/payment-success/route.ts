e9015d536863da7262caebaedade71895d030efa
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2024-04-11 11:55:17 +0200

    get function

--- src/app/api/payment-success/route.ts
+++ src/app/api/payment-success/route.ts
@@ -1,35 +1,50 @@
-// Файл: pages/api/payment-success/[sessionId].ts
+// Файл: app/api/payment-success/route.ts
 
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   apiVersion: '2023-10-16',
 });
 
-export default async function(req: NextApiRequest, res: NextApiResponse) {
-  const { sessionId } = req.query;
+export async function GET(req: NextRequest) {
+  const sessionId = req.nextUrl.searchParams.get('sessionId');
 
-  if (!sessionId || typeof sessionId !== 'string') {
-    return res.status(400).json({ error: 'Bad Request' });
+  if (!sessionId) {
+    return new NextResponse(JSON.stringify({ error: 'Bad Request' }), {
+      status: 400,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 
   try {
-    // Проверяем сессию оплаты в Stripe, чтобы подтвердить успешность оплаты
     const session = await stripe.checkout.sessions.retrieve(sessionId);
 
     if (session.payment_status === 'paid') {
-      // Здесь может быть логика для создания или получения уникального URL для курса на основе stripePurchaseId
-      // Например, вы можете сохранить stripePurchaseId в базе данных и ассоциировать его с курсом и пользователем
-      // В этом примере мы просто перенаправляем на статический URL курса для демонстрации
       const courseAccessUrl = `https://landi-academy.pl/kurs/${sessionId}`;
-      res.status(200).json({ success: true, courseAccessUrl });
+      return new NextResponse(JSON.stringify({ success: true, courseAccessUrl }), {
+        status: 200,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
     } else {
-      // Обработка случая, когда платёж не был успешен
-      res.status(403).json({ error: 'Payment not successful' });
+      return new NextResponse(JSON.stringify({ error: 'Payment not successful' }), {
+        status: 403,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
     }
   } catch (error) {
     console.error('Error retrieving Stripe session:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    return new NextResponse(JSON.stringify({ error: 'Internal Server Error' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 }
